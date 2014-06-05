baf0587b534eef0b27f5f74d91182d8c0043f9f3
ke-pan <ke_pan@yeah.net>
2014-06-05 21:52:21 +0800

    Finish basic CRU not D

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: :user_id, class_name: "User"
   has_many :comments
   has_and_belongs_to_many :categories
+
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
 end
